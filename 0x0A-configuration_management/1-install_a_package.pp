16dd49231ac218f85e0230d92845a4c61f745cb1
Wmugacha <wilfredmugacha@gmail.com>
2023-03-28 07:56:24 +0000

    install flask using puppet manifest

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,7 +4,9 @@ exec { 'pip3':
     command => '/usr/bin/sudo apt-get -y install python3-pip'
 }
 
-exec { 'flask':
-    command => '/usr/bin/pip3 install Flask',
-    require => Exec['pip3']
+package { 'flask':
+    ensure   => '2.1.0',
+    name     => 'flask',
+    provider => 'pip3',
+    require  => Exec['pip3']
 }
