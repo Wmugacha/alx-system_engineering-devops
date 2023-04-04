3e67da9fc2c49ecfd1bbf5a66d4dab4611b8ecd5
Wmugacha <wilfredmugacha@gmail.com>
2023-04-04 18:21:24 +0000

    custom header using puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -7,7 +7,7 @@ exec {'get-update':
     ensure => 'present',
 }
 -> file_line {'http_header':
-    path  => 'etc/nginx/nginx.conf',
+    path  => '/etc/nginx/nginx.conf',
     match => 'http {',
     line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
