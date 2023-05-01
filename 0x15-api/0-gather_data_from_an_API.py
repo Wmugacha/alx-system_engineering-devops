473a1a73650124776652956adcb30e3f509f512a
Wmugacha <wilfredmugacha@gmail.com>
2023-05-01 05:51:47 +0000

    script to give todo list progress of an employee

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" Script that, using this REST API, for a given employee ID, returns
-    information about his/her TODO list progress """
+"""fetch json data from an API"""
 
 import requests
 import sys
