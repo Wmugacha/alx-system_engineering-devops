487487ca8b77188cf3d1f935b2842b852461078f
Wmugacha <wilfredmugacha@gmail.com>
2023-05-01 06:16:12 +0000

    script to give todo list progress of an employee

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,19 +1,7 @@
 #!/usr/bin/python3
-"""This script fetches JSON data from an API to retrieve information about a given employee's progress on their TODO list.
-
-Usage:
-    python3 employee_progress.py [employee_id]
-
-Arguments:
-    employee_id: An integer representing the ID of the employee whose progress you wish to retrieve.
-
-API:
-    The script utilizes the jsonplaceholder.typicode.com API to retrieve employee and task information.
-
-Returns:
-    The script returns the name of the employee and the number of tasks completed
-	 out of the total number of tasks. Additionally, the script prints out the titles of the completed tasks.
-
+"""
+This script fetches JSON data from an API to retrieve information
+about a given employee's progress on their TODO list.
 """
 
 import requests
