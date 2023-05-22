fff1fb5a3e079f0d690ff4112e7e298ec82f5ade
Wmugacha <wilfredmugacha@gmail.com>
2023-05-22 12:46:14 +0000

    get employee to-do list progress

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This script fetches JSON data from an API to retrieve information
+about a given employee's progress on their TODO list.
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    emp_id = int(argv[1])
+
+    response = requests.get(
+            f'https://jsonplaceholder.typicode.com/users/{emp_id}')
+
+    data = response.json()
+
+    name = data['name']
+
+    response = requests.get(
+            f'https://jsonplaceholder.typicode.com/todos?userId={emp_id}')
+
+    todos = response.json()
+
+    completed_todos = sum(todo['completed'] for todo in todos)
+
+    total_todos = len(todos)
+
+    print(f"Employee {name} is done with tasks"
+          f"({completed_todos}/{total_todos}):")
+
+    for todo in todos:
+        if todo['completed']:
+            print('\t' + ' ' + todo['title'])
