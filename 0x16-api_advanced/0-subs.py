c76e73680b2911a126212d4448afe046fa824623
Wmugacha <wilfredmugacha@gmail.com>
2023-06-07 06:27:36 +0000

    query number of subreddit subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,11 +12,13 @@ def number_of_subscribers(subreddit):
 
     url = (f"https://www.reddit.com/r/{subreddit}/about.json")
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
+
         subs = data["data"]["subscribers"]
+
         return subs
 
     else:
