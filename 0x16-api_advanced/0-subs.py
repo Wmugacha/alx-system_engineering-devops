45f55500e66636475585e6597971154291ae23df
Wmugacha <wilfredmugacha@gmail.com>
2023-06-07 06:14:13 +0000

    query number of subreddit subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,18 +9,11 @@ def number_of_subscribers(subreddit):
     """ Function that gives the number of subscribers
     in a subreddit.
     """
-    client_auth = requests.auth.HTTPBasicAuth('DMIGUkVh_PwUgc6rtLzJBA',
-                                              '2T2VPdBleJFmc-KayPse4RD6o839fw')
-
-    post_data = {"grant_type": "password", "username": "proudprune",
-                 "password": "naruto95"}
-
-    headers = {"User-Agent": "ChangeMeClient/0.1 by proudprune"}
+    headers = {"User-Agent": "My Reddit Script"}
 
     url = (f"https://www.reddit.com/r/{subreddit}/about.json")
 
-    response = requests.get(url, auth=client_auth, data=post_data,
-                            headers=headers)
+    response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
