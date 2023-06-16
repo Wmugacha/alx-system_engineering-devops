7477b54157b1ec80df39658a584cc77d00d39da9
Wmugacha <wilfredmugacha@gmail.com>
2023-06-16 10:00:55 +0000

    holberton user limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,8 +1,8 @@
 # Rectify holberton user process limit
 
 file_line { 'holberton_limits':
-  path    => '/etc/security/limits.conf',
-  line    => ['holberton hard nofile 4096', 'holberton soft nofile 4096'],
-  match   => '^holberton',
-  ensure  => present,
+  ensure => present,
+  path   => '/etc/security/limits.conf',
+  line   => ['holberton hard nofile 4096', 'holberton soft nofile 4096'],
+  match  => '^holberton',
 }
